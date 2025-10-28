f6405cdee5e6c61533616830f5edc73645e8617a
Cristian <cristian.martinez.716cm9@gmail.com>
2025-10-27 19:22:19 -0600

    feat: Add preview configuration with allowed hosts and port settings in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'tranquil-fascination-production.up.railway.app',
+      '.railway.app', // Permite todos los subdominios de railway.app
+    ],
+    host: '0.0.0.0',
+    port: process.env.PORT || 8080,
+  },
 })
